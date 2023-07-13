cb15871dd90933a38d09ae4b4597edb2f88ba284
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-07-13 15:02:24 -0300

    chore: add eslint setups

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 
-import "../styles/global.scss";
+import "styles/global.scss";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
