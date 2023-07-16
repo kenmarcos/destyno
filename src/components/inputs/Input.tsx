2b22827e25a2e1f6a5a599bf689ae175f26f245c
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-07-16 00:42:20 -0300

    feat: add input component

---
+++ src/components/inputs/Input.tsx
@@ -0,0 +1,25 @@
+import { ComponentPropsWithoutRef, LegacyRef, forwardRef } from "react";
+
+import styles from "./Input.module.scss";
+
+interface InputProps extends ComponentPropsWithoutRef<"input"> {
+  error?: boolean;
+  errorMessage?: string;
+}
+
+const Input = (
+  { className = "", error = false, errorMessage = "", ...rest }: InputProps,
+  ref: LegacyRef<HTMLInputElement> | undefined
+) => {
+  return (
+    <div className={styles.input}>
+      <input {...rest} ref={ref} />
+
+      {error && errorMessage && (
+        <small className={styles.error}>{errorMessage}</small>
+      )}
+    </div>
+  );
+};
+
+export default forwardRef(Input);
