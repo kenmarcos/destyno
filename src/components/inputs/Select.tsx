78702f26c223d53b0dd2fdbd8937b5f73135de9e
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-07-17 15:11:44 -0300

    feat: add select component

---
+++ src/components/inputs/Select.tsx
@@ -0,0 +1,40 @@
+import { ComponentPropsWithoutRef, LegacyRef, forwardRef } from "react";
+
+import styles from "./Select.module.scss";
+
+interface SelectProps extends ComponentPropsWithoutRef<"select"> {
+  error?: boolean;
+  errorMessage?: string;
+  options: { value: string; label: string }[];
+}
+
+const Select = (
+  {
+    className = "",
+    error = false,
+    errorMessage = "",
+    options,
+    ...rest
+  }: SelectProps,
+  ref: LegacyRef<HTMLSelectElement> | undefined
+) => {
+  return (
+    <div className={styles.select}>
+      <select {...rest} ref={ref}>
+        <option value="selecione">Selecione</option>
+
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+
+      {error && errorMessage && (
+        <small className={styles.error}>{errorMessage}</small>
+      )}
+    </div>
+  );
+};
+
+export default forwardRef(Select);
